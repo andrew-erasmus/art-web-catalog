357fc9c1ab420ee8cda7bb8342b71364322a8f1b
andrew-erasmus <andrew.r.eras@gmail.com>
2023-10-02 16:43:42 +0200

    Add moving menu icon with functionality

--- script.js
+++ script.js
@@ -1,16 +1,45 @@
-function dropDown() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
+// Get the checkbox element
+var checkbox = document.getElementById("checkbox");
+
+// Get the dropdown menu element
+var myDropdown = document.getElementById("myDropdown");
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function (e) {
-    if (!e.target.matches('.dropbtn')) {
-        var myDropdown = document.getElementById("myDropdown");
+    if(myDropdown.classList.contains('window')){
+        myDropdown.classList.remove('window');
+    }else if (!e.target.matches('.dropbtn') || !e.target.matches('#checkbox')) {
         if (myDropdown.classList.contains('show')) {
             myDropdown.classList.remove('show');
+            myDropdown.classList.add('window');
+            checkbox.checked=false;
         }
     }
 }
+
+// Add an event listener for the 'change' event
+checkbox.addEventListener('change', function() {
+    // Toggle the 'show' class based on the checked property of the checkbox
+    if(myDropdown.classList.contains("window") && checkbox.checked){
+        myDropdown.classList.remove('window');
+        checkbox.checked=false;
+    }
+    if(myDropdown.classList.contains("window") && checkbox.checked == false){
+        myDropdown.classList.remove('window');
+        myDropdown.classList.add("show");
+        checkbox.checked=true;
+    }
+
+    if (checkbox.checked) {
+        myDropdown.classList.add("show");
+    } else {
+        myDropdown.classList.remove("show");
+    }
+    
+});
+
+
+
 
 // Carousel functionality
 const buttons = document.querySelectorAll("[data-carousel-button]")
